bfc5c04afa8e6098b7fcf44ca644ef91b5d85e1c
Kamil Pietrucha <kamil7.pl@gmail.com>
2021-07-13 11:13:35 +0200

    subpages

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,7 +24,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    <img src="{{asset('images/k1.jpg')}}" alt="k1" width="30" height="24">
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -33,7 +33,12 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{asset('subpageone')}}">subpageone</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{asset('subpagetwo')}}">subpagetwo</a>
+                        </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
