22ac258cead1308b7ff42a03c007e02265a936d0
Kamil Pietrucha <kamil7.pl@gmail.com>
2021-07-13 12:35:42 +0200

    asset from app

--- resources/views/grid/index.blade.php
+++ resources/views/grid/index.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
-    <link href="{{asset('bootstrap5/css/bootstrap.min.css')}}" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{asset('css/kamilCSS/style.css')}}" rel="stylesheet">
 
     <title>Hello, world!</title>
