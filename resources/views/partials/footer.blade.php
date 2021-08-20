7b7d018cf0fa8cc20c2cf09b7b1dad2eaa86070e
Kamil Pietrucha <kamil7.pl@gmail.com>
2021-08-20 22:38:56 +0200

    poprawka

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,1 +1,2 @@
 <div class="p-3 mb-2 bg-light text-dark text-center">Footer</div>
+</div>
---
+++ resources/views/partials/head.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="{{asset('/bootstrap5/css/bootstrap.min.css')}}" rel="stylesheet">
+
+    <title>Laravel</title>
+  </head>
+  <body>
+    <div class="container-fluid">
