878535ebd08af3d4adfc96957317f8689a417663
Kamil Pietrucha <kamil7.pl@gmail.com>
2021-08-28 16:45:39 +0200

    m

---
+++ resources/views/p24/index.blade.php
@@ -0,0 +1,9 @@
+@include('partials.head')
+        @include('partials.navbar')
+          <div class="content">
+              <form action="{{$url}}" method="post"><!-- Można użyć Laravel collective do formularza.-->
+                <input type="submit" value="ok" />
+              </form>
+          </div>
+          @include('partials.footer')
+          @include('partials.scripts')
