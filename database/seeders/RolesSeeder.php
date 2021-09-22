f1412fa3b4471d69242eedc88a39f1cfa27886b3
Kamil Pietrucha <kamil7.pl@gmail.com>
2021-09-22 20:01:59 +0200

    cwiczenia

---
+++ database/seeders/RolesSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Role;
+
+class RolesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $roles = [
+            [
+                'name' => 'admin'
+            ],
+            [
+                'name' => 'user'
+            ]
+        ];
+        foreach($roles as $role)
+            Role::create($role);
+    }
+}
