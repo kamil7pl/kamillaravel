f0bfa79eebf672c4b74c7b7d4a1468cf8ab2b9e0
Kamil Pietrucha <kamil7.pl@gmail.com>
2021-09-22 17:28:07 +0200

    Seedery dla userow haslo password

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Str;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $users = [
+            [
+                'name' => 'kamil7',
+                'email' => 'kamil7.pl@gmail.com',
+                'email_verified_at' => now(),
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+                'remember_token' => Str::random(10)
+            ],
+            [
+                'name' => 'michal7',
+                'email' => 'michal7.pl@gmail.com',
+                'email_verified_at' => now(),
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+                'remember_token' => Str::random(10)
+            ],
+            [
+                'name' => 'pawel7',
+                'email' => 'pawel7.pl@gmail.com',
+                'email_verified_at' => now(),
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+                'remember_token' => Str::random(10)
+            ]
+        ];
+        foreach($users as $user)
+        {
+            User::create($user);
+        }
+    }
+}
